5320ac43d8a7dff2fc88f9720064a0cac283c339
brain-fuel <matt@brain-fuel.co>
2024-12-07 18:34:24 -0500

    implement near misses

--- predicates/interface.go
+++ predicates/interface.go
@@ -2,6 +2,7 @@ package predicates
 
 import (
 	"errors"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -17,9 +18,9 @@ const (
 
 func NearMissThresholds() (int, float64) {
 	lookupCharThreshold, ctExists := os.LookupEnv("NEAR_MISS_CHAR_THRESHOLD")
-	lookupPercentThreshold, ptExists := os.LookupEnv("NEAR_MISS_DECIMAL_THRESHOLD")
-	var ct int
-	var pt float64
+	lookupPercentThreshold, dtExists := os.LookupEnv("NEAR_MISS_DECIMAL_THRESHOLD")
+	ct := NEAR_MISS_CHAR_THRESHOLD
+	dt := NEAR_MISS_DECIMAL_THRESHOLD
 	if ctExists {
 		lct, err := strconv.Atoi(lookupCharThreshold)
 		if err != nil {
@@ -28,15 +29,24 @@ func NearMissThresholds() (int, float64) {
 			ct = lct
 		}
 	}
-	if ptExists {
-		lpt, err := strconv.ParseFloat(lookupPercentThreshold, 64)
+	if dtExists {
+		ldt, err := strconv.ParseFloat(lookupPercentThreshold, 64)
 		if err != nil {
-			pt = NEAR_MISS_DECIMAL_THRESHOLD
+			dt = NEAR_MISS_DECIMAL_THRESHOLD
 		} else {
-			pt = lpt
+			dt = ldt
 		}
 	}
-	return ct, pt
+	return ct, dt
+}
+
+func NearMissThreshold(length int) int {
+	ct, dt := NearMissThresholds()
+	isShort := length <= 4
+	if isShort {
+		return ct
+	}
+	return int(math.Floor((dt * float64(length))))
 }
 
 func EOF() t.MatchPred {
@@ -59,6 +69,20 @@ func AnyRune() t.MatchPred {
 			butFirstCharInString(in),
 		)
 	}
+}
+
+func firstNRunesInString(n int, s string) string {
+	if n <= 0 {
+		return ""
+	}
+	count := 0
+	for idx := range s {
+		count++
+		if n < count {
+			return s[:idx]
+		}
+	}
+	return s
 }
 
 func firstRuneInString(cs string) (rune, error) {
@@ -129,19 +153,39 @@ func Str(toMatch string) t.MatchPred {
 	return func(in string) t.MatchRes {
 		inputLength := utf8.RuneCountInString(in)
 		matchLength := utf8.RuneCountInString(toMatch)
-		dldistLength := matchLength
-		dldist := DLDist(in, toMatch[:dldistLength])
-		odldist := ODLDist(in, toMatch[:dldistLength])
-		eof := inputLength < matchLength
+		matchCut := firstNRunesInString(matchLength, toMatch)
+		inputCut := firstNRunesInString(matchLength, in)
+		dldist := DLDist(matchCut, inputCut)
+		odldist := ODLDistTest(matchCut, inputCut)
+		eof := inputLength == 0
 		success := dldist == 0
 		if eof {
 			return t.NewMatchFailure(t.FAILURE_EOF, dldist, odldist, in)
 		}
 		if !success {
-			return t.NewMatchFailure(t.FAILURE_NO_MATCH, dldist, odldist, in)
+			matchType := failMatchType(matchLength, dldist, odldist)
+			return t.NewMatchFailure(matchType, dldist, odldist, in)
 		}
 		return t.NewMatchSuccess(t.SUCCESS_STRING, toMatch, butNCharInString(matchLength, in))
 	}
+}
+
+func failMatchType(length int, dldist int, odldist t.Pair[int]) t.MatchType {
+	nearMissThreshold := NearMissThreshold(length)
+	ocount, lacking := odldist.ToTuple()
+	if 0 < lacking {
+		if ocount == 0 {
+			return t.FAILURE_MATCH_THEN_EOF
+		}
+		if ocount <= nearMissThreshold {
+			return t.FAILURE_NEAR_MISS_THEN_EOF
+		}
+		return t.FAILURE_NO_MATCH_THEN_EOF
+	}
+	if dldist <= nearMissThreshold {
+		return t.FAILURE_NEAR_MISS
+	}
+	return t.FAILURE_NO_MATCH
 }
 
 func absInt(n int) int {
@@ -162,34 +206,34 @@ func DLDist(s1, s2 string) int {
 	if s1 == s2 {
 		return 0
 	}
-	s1Array := strings.Split(s1, "")
-	s2Array := strings.Split(s2, "")
-	lenS1Array := len(s1Array)
-	lenS2Array := len(s2Array)
-	m := make([][]int, lenS1Array+1)
+	rs1 := strings.Split(s1, "")
+	rs2 := strings.Split(s2, "")
+	l1 := len(rs1)
+	l2 := len(rs2)
+	m := make([][]int, l1+1)
 	var cost int
 	for i := range m {
-		m[i] = make([]int, lenS2Array+1)
+		m[i] = make([]int, l2+1)
 	}
-	for i := 0; i < lenS1Array+1; i++ {
-		for j := 0; j < lenS2Array+1; j++ {
+	for i := 0; i < l1+1; i++ {
+		for j := 0; j < l2+1; j++ {
 			if i == 0 {
 				m[i][j] = j
 			} else if j == 0 {
 				m[i][j] = i
 			} else {
 				cost = 0
-				if s1Array[i-1] != s2Array[j-1] {
+				if rs1[i-1] != rs2[j-1] {
 					cost = 1
 				}
 				m[i][j] = multiMin(m[i-1][j]+1, m[i][j-1]+1, m[i-1][j-1]+cost)
-				if i > 1 && j > 1 && s1Array[i-1] == s2Array[j-2] && s1Array[i-2] == s2Array[j-1] {
+				if i > 1 && j > 1 && rs1[i-1] == rs2[j-2] && rs1[i-2] == rs2[j-1] {
 					m[i][j] = multiMin(m[i][j], m[i-2][j-2]+cost)
 				}
 			}
 		}
 	}
-	return m[lenS1Array][lenS2Array]
+	return m[l1][l2]
 }
 
 func multiMin(ns ...int) int {
@@ -212,4 +256,12 @@ func ODLDist(s1, s2 string) t.Pair[int] {
 	minLen := min(l1, l2)
 	diff := absInt(l1 - l2)
 	return t.NewPair[int](DLDist(s1[:minLen], s2[:minLen]), diff)
+}
+
+func ODLDistTest(expected, actual string) t.Pair[int] {
+	lExpected := utf8.RuneCountInString(expected)
+	lActual := utf8.RuneCountInString(actual)
+	minLen := min(lExpected, lActual)
+	diff := lExpected - lActual
+	return t.NewPair[int](DLDist(expected[:minLen], actual[:minLen]), diff)
 }
--- predicates/interface_test.go
+++ predicates/interface_test.go
@@ -5,12 +5,12 @@ import (
 )
 
 func ExampleEOF() {
-	toFail := "abacadaba"
+	failureInput := "abacadaba"
 
-	toSucceed := ""
+	successfulInput := ""
 
-	expectedFailMatch := EOF()(toFail)
-	expectedSuccessMatch := EOF()(toSucceed)
+	expectedFailMatch := EOF()(failureInput)
+	expectedSuccessMatch := EOF()(successfulInput)
 
 	fmt.Println(expectedFailMatch)
 	fmt.Printf("%v\n", expectedSuccessMatch)
@@ -19,16 +19,16 @@ func ExampleEOF() {
 }
 
 func ExampleAnyRune() {
-	toFail := ""
+	failureInput := ""
 
-	toSucceed1 := "abacadaba"
-	toSucceed2 := "123abc"
-	toSucceed3 := "世界"
+	successfulInput1 := "abacadaba"
+	successfulInput2 := "123abc"
+	successfulInput3 := "世界"
 
-	expectedFailMatch := AnyRune()(toFail)
-	expectedSuccessMatch1 := AnyRune()(toSucceed1)
-	expectedSuccessMatch2 := AnyRune()(toSucceed2)
-	expectedSuccessMatch3 := AnyRune()(toSucceed3)
+	expectedFailMatch := AnyRune()(failureInput)
+	expectedSuccessMatch1 := AnyRune()(successfulInput1)
+	expectedSuccessMatch2 := AnyRune()(successfulInput2)
+	expectedSuccessMatch3 := AnyRune()(successfulInput3)
 
 	fmt.Println(expectedFailMatch)
 	fmt.Println(expectedSuccessMatch1)
@@ -44,15 +44,15 @@ func ExampleAnyRune() {
 func ExampleRune() {
 	toMatch := '世'
 
-	toFail1 := ""
-	toFail2 := "abacadaba"
-	toFail3 := "123abc"
-	toSucceed := "世界"
+	failureInput1 := ""
+	failureInput2 := "abacadaba"
+	failureInput3 := "123abc"
+	successfulInput := "世界"
 
-	expectedFailMatch1 := Rune(toMatch)(toFail1)
-	expectedFailMatch2 := Rune(toMatch)(toFail2)
-	expectedFailMatch3 := Rune(toMatch)(toFail3)
-	expectedSuccessMatch := Rune(toMatch)(toSucceed)
+	expectedFailMatch1 := Rune(toMatch)(failureInput1)
+	expectedFailMatch2 := Rune(toMatch)(failureInput2)
+	expectedFailMatch3 := Rune(toMatch)(failureInput3)
+	expectedSuccessMatch := Rune(toMatch)(successfulInput)
 
 	fmt.Println(expectedFailMatch1)
 	fmt.Println(expectedFailMatch2)
@@ -68,74 +68,87 @@ func ExampleRune() {
 func ExampleStr() {
 	toMatch1 := "世界"
 
-	toFail1_1 := ""
-	toFail1_2 := "abacadaba"
-	toFail1_3 := "123abc"
-	toFail1_4 := "世"
-	toFail1_5 := "界"
-	expectedFailMatch1_1 := Str(toMatch1)(toFail1_1)
-	expectedFailMatch1_2 := Str(toMatch1)(toFail1_2)
-	expectedFailMatch1_3 := Str(toMatch1)(toFail1_3)
-	expectedFailMatch1_4 := Str(toMatch1)(toFail1_4)
-	expectedFailMatch1_5 := Str(toMatch1)(toFail1_5)
-	fmt.Println(expectedFailMatch1_1)
-	fmt.Println(expectedFailMatch1_2)
-	fmt.Println(expectedFailMatch1_3)
-	fmt.Println(expectedFailMatch1_4)
-	fmt.Println(expectedFailMatch1_5)
+	failureInput1_1 := ""
+	expectedFailMatch1_1 := Str(toMatch1)(failureInput1_1)
+	fmt.Printf("Failure: %v\n", expectedFailMatch1_1)
 
-	toSucceed1_1 := "世界"
-	toSucceed1_2 := "世界abacadaba"
-	toSucceed1_3 := "世界123abc"
-	expectedSuccessMatch1_1 := Str(toMatch1)(toSucceed1_1)
-	expectedSuccessMatch1_2 := Str(toMatch1)(toSucceed1_2)
-	expectedSuccessMatch1_3 := Str(toMatch1)(toSucceed1_3)
-	fmt.Println(expectedSuccessMatch1_1)
-	fmt.Println(expectedSuccessMatch1_2)
-	fmt.Println(expectedSuccessMatch1_3)
+	failureInput1_2 := "abacadaba"
+	expectedFailMatch1_2 := Str(toMatch1)(failureInput1_2)
+	fmt.Printf("Failure: %v\n", expectedFailMatch1_2)
 
+	failureInput1_3 := "123abc"
+	expectedFailMatch1_3 := Str(toMatch1)(failureInput1_3)
+	fmt.Printf("Failure: %v\n", expectedFailMatch1_3)
+
+	failureInput1_4 := "世"
+	expectedFailMatch1_4 := Str(toMatch1)(failureInput1_4)
+	fmt.Printf("Failure: %v\n", expectedFailMatch1_4)
+
+	failureInput1_5 := "界"
+	expectedFailMatch1_5 := Str(toMatch1)(failureInput1_5)
+	fmt.Printf("Failure: %v\n", expectedFailMatch1_5)
+
+	successfulInput1_1 := "世界"
+	expectedSuccessMatch1_1 := Str(toMatch1)(successfulInput1_1)
+	fmt.Printf("Success: %v\n", expectedSuccessMatch1_1)
+
+	successfulInput1_2 := "世界abacadaba"
+	expectedSuccessMatch1_2 := Str(toMatch1)(successfulInput1_2)
+	fmt.Printf("Success: %v\n", expectedSuccessMatch1_2)
+
+	successfulInput1_3 := "世界123abc"
+	expectedSuccessMatch1_3 := Str(toMatch1)(successfulInput1_3)
+	fmt.Printf("Success: %v\n", expectedSuccessMatch1_3)
+
+	// --------------------
 	toMatch2 := "secondmatch"
 
-	toFail2_1 := ""
-	toFail2_2 := "abacadaba"
-	toFail2_3 := "secondm@tc"
-	toFail2_4 := "secondm@tch"
-	toFail2_5 := "secondm@tch123abc"
-	expectedFailMatch2_1 := Str(toMatch2)(toFail2_1)
-	expectedFailMatch2_2 := Str(toMatch2)(toFail2_2)
-	expectedFailMatch2_3 := Str(toMatch2)(toFail2_3)
-	expectedFailMatch2_4 := Str(toMatch2)(toFail2_4)
-	expectedFailMatch2_5 := Str(toMatch2)(toFail2_5)
-	fmt.Println(expectedFailMatch2_1)
-	fmt.Println(expectedFailMatch2_2)
-	fmt.Println(expectedFailMatch2_3)
-	fmt.Println(expectedFailMatch2_4)
-	fmt.Println(expectedFailMatch2_5)
+	failureInput2_1 := ""
+	expectedFailMatch2_1 := Str(toMatch2)(failureInput2_1)
+	fmt.Printf("Failure: %v\n", expectedFailMatch2_1)
 
-	toSucceed2_1 := "secondmatch"
-	toSucceed2_2 := "secondmatch123"
-	toSucceed2_3 := "secondmatchabacadaba"
-	expectedSuccessMatch2_1 := Str(toMatch2)(toSucceed2_1)
-	expectedSuccessMatch2_2 := Str(toMatch2)(toSucceed2_2)
-	expectedSuccessMatch2_3 := Str(toMatch2)(toSucceed2_3)
-	fmt.Println(expectedSuccessMatch2_1)
-	fmt.Println(expectedSuccessMatch2_2)
-	fmt.Println(expectedSuccessMatch2_3)
+	failureInput2_2 := "abacadaba"
+	expectedFailMatch2_2 := Str(toMatch2)(failureInput2_2)
+	fmt.Printf("Failure: %v\n", expectedFailMatch2_2)
 
-	// Output: MatchRes{type: FAILURE_EOF, dldist: 2, odldist: {0 2}, match: "", rest: ""}
-	// MatchRes{type: FAILURE_NO_MATCH, dldist: 9, odldist: {2 7}, match: "", rest: "abacadaba"}
-	// MatchRes{type: FAILURE_NO_MATCH, dldist: 6, odldist: {2 4}, match: "", rest: "123abc"}
-	// MatchRes{type: FAILURE_EOF, dldist: 2, odldist: {0 1}, match: "", rest: "世"}
-	// MatchRes{type: FAILURE_EOF, dldist: 2, odldist: {1 1}, match: "", rest: "界"}
-	// MatchRes{type: FAILURE_NO_MATCH, dldist: 2, odldist: {0 0}, match: "", rest: "世界"}
-	// MatchRes{type: FAILURE_NO_MATCH, dldist: 11, odldist: {0 9}, match: "", rest: "世界abacadaba"}
-	// MatchRes{type: FAILURE_NO_MATCH, dldist: 8, odldist: {0 6}, match: "", rest: "世界123abc"}
-	// MatchRes{type: FAILURE_EOF, dldist: 11, odldist: {0 11}, match: "", rest: ""}
-	// MatchRes{type: FAILURE_EOF, dldist: 9, odldist: {8 2}, match: "", rest: "abacadaba"}
-	// MatchRes{type: FAILURE_EOF, dldist: 2, odldist: {1 1}, match: "", rest: "secondm@tc"}
-	// MatchRes{type: FAILURE_NO_MATCH, dldist: 1, odldist: {1 0}, match: "", rest: "secondm@tch"}
-	// MatchRes{type: FAILURE_NO_MATCH, dldist: 7, odldist: {1 6}, match: "", rest: "secondm@tch123abc"}
-	// MatchRes{type: SUCCESS_STRING, dldist: 0, odldist: {0 0}, match: "secondmatch", rest: ""}
-	// MatchRes{type: FAILURE_NO_MATCH, dldist: 3, odldist: {0 3}, match: "", rest: "secondmatch123"}
-	// MatchRes{type: FAILURE_NO_MATCH, dldist: 9, odldist: {0 9}, match: "", rest: "secondmatchabacadaba"}
+	failureInput2_3 := "secondm@tc"
+	expectedFailMatch2_3 := Str(toMatch2)(failureInput2_3)
+	fmt.Printf("Failure: %v\n", expectedFailMatch2_3)
+
+	failureInput2_4 := "secondm@tch"
+	expectedFailMatch2_4 := Str(toMatch2)(failureInput2_4)
+	fmt.Printf("Failure: %v\n", expectedFailMatch2_4)
+
+	failureInput2_5 := "secondm@tch123abc"
+	expectedFailMatch2_5 := Str(toMatch2)(failureInput2_5)
+	fmt.Printf("Failure: %v\n", expectedFailMatch2_5)
+
+	successfulInput2_1 := "secondmatch"
+	expectedSuccessMatch2_1 := Str(toMatch2)(successfulInput2_1)
+	fmt.Printf("Success: %v\n", expectedSuccessMatch2_1)
+
+	successfulInput2_2 := "secondmatch123"
+	expectedSuccessMatch2_2 := Str(toMatch2)(successfulInput2_2)
+	fmt.Printf("Success: %v\n", expectedSuccessMatch2_2)
+
+	successfulInput2_3 := "secondmatchabacadaba"
+	expectedSuccessMatch2_3 := Str(toMatch2)(successfulInput2_3)
+	fmt.Printf("Success: %v\n", expectedSuccessMatch2_3)
+
+	// Output: Failure: MatchRes{type: FAILURE_EOF, dldist: 2, odldist: {0 2}, match: "", rest: ""}
+	// Failure: MatchRes{type: FAILURE_NO_MATCH, dldist: 2, odldist: {2 0}, match: "", rest: "abacadaba"}
+	// Failure: MatchRes{type: FAILURE_NO_MATCH, dldist: 2, odldist: {2 0}, match: "", rest: "123abc"}
+	// Failure: MatchRes{type: FAILURE_MATCH_THEN_EOF, dldist: 1, odldist: {0 1}, match: "", rest: "世"}
+	// Failure: MatchRes{type: FAILURE_NEAR_MISS_THEN_EOF, dldist: 1, odldist: {1 1}, match: "", rest: "界"}
+	// Success: MatchRes{type: SUCCESS_STRING, dldist: 0, odldist: {0 0}, match: "世界", rest: ""}
+	// Success: MatchRes{type: SUCCESS_STRING, dldist: 0, odldist: {0 0}, match: "世界", rest: "abacadaba"}
+	// Success: MatchRes{type: SUCCESS_STRING, dldist: 0, odldist: {0 0}, match: "世界", rest: "123abc"}
+	// Failure: MatchRes{type: FAILURE_EOF, dldist: 11, odldist: {0 11}, match: "", rest: ""}
+	// Failure: MatchRes{type: FAILURE_NO_MATCH_THEN_EOF, dldist: 9, odldist: {8 2}, match: "", rest: "abacadaba"}
+	// Failure: MatchRes{type: FAILURE_NEAR_MISS_THEN_EOF, dldist: 2, odldist: {1 1}, match: "", rest: "secondm@tc"}
+	// Failure: MatchRes{type: FAILURE_NEAR_MISS, dldist: 1, odldist: {1 0}, match: "", rest: "secondm@tch"}
+	// Failure: MatchRes{type: FAILURE_NEAR_MISS, dldist: 1, odldist: {1 0}, match: "", rest: "secondm@tch123abc"}
+	// Success: MatchRes{type: SUCCESS_STRING, dldist: 0, odldist: {0 0}, match: "secondmatch", rest: ""}
+	// Success: MatchRes{type: SUCCESS_STRING, dldist: 0, odldist: {0 0}, match: "secondmatch", rest: "123"}
+	// Success: MatchRes{type: SUCCESS_STRING, dldist: 0, odldist: {0 0}, match: "secondmatch", rest: "abacadaba"}
 }
